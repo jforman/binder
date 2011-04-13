7314c6daf8fad5f97757c73cea207c1783764c5b
Jeffrey Forman <jeff@jeffreyforman.net>
2011-04-13 19:17:48 -0400

    Make the admin UI more useful showing fields in key and zone

--- binder/bcommon/admin.py
+++ binder/bcommon/admin.py
@@ -1,6 +1,12 @@
 from bcommon.models import BindServer, Key, Zone
 from django.contrib import admin
 
+class ZoneAdmin(admin.ModelAdmin):
+    list_display = [ 'zone_name',  'server']
+
+class KeyAdmin(admin.ModelAdmin):
+    list_display = [ 'name', 'data' ]
+
 admin.site.register(BindServer)
-admin.site.register(Key)
-admin.site.register(Zone)
+admin.site.register(Key, KeyAdmin)
+admin.site.register(Zone, ZoneAdmin)
