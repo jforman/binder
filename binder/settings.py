ae0830436afb04f67a04fe3ab87c0d3c80a38dda
Jeffrey Forman <jforman@malbec.home.jeffreyforman.net>
2012-11-08 22:59:40 -0500

    remove sites installed app that isnt used

--- binder/settings.py
+++ binder/settings.py
@@ -68,7 +68,6 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.admin',
     'django.contrib.staticfiles',
