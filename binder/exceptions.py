823da81ed4016fdac6e20b9e94f59eeee79f9990
Jeffrey Forman <jforman@malbec.home.jeffreyforman.net>
2012-11-17 20:40:15 -0500

    Add exceptions for records and zone modifications

--- binder/exceptions.py
+++ binder/exceptions.py
@@ -6,3 +6,20 @@ class TransferException(Exception):
     """
 
     pass
+
+class ZoneException(Exception):
+    """
+    Thrown when there is an issue dealing with a
+    DNS zone.
+    """
+
+    pass
+
+class RecordException(Exception):
+    """
+    Thrown when there is an issue dealign with
+    a Record.
+      * Adding or deleting.
+    """
+
+    pass
