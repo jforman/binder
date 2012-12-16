a7bc198c4d0190f0532380129fcebb6901d37d56
Jeffrey Forman <jforman@malbec.home.jeffreyforman.net>
2012-12-15 20:25:06 -0500

    Convert template URL href references to use 'url' template syntax for reverse URLs

--- binder/urls.py
+++ binder/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.core.urlresolvers import reverse
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,15 +9,15 @@ urlpatterns = patterns('',
     url(r'^$', 'binder.views.home_index', name="index"),
     url(r'^server_list/$', 'binder.views.view_server_list', name="server_list"),
 
-    url(r'^info/(?P<dns_server>[a-zA-Z0-9.-]+)/$', 'binder.views.view_server_zones', name="server_zones"),
-    url(r'^info/(?P<dns_server>[a-zA-Z0-9.-]+)/(?P<zone_name>[a-zA-Z0-9.-]+)/$', 'binder.views.view_zone_records', name="zone_records"),
+    url(r'^info/(?P<dns_server>[a-zA-Z0-9.-]+)/$', 'binder.views.view_server_zones', name="server_zone_list"),
+    url(r'^info/(?P<dns_server>[a-zA-Z0-9.-]+)/(?P<zone_name>[a-zA-Z0-9.-]+)/$', 'binder.views.view_zone_records', name="zone_list"),
 
-    url(r'^add_record/(?P<dns_server>[a-zA-Z0-9.-]+)/(?P<zone_name>[a-zA-Z0-9.-]+)/$', 'binder.views.view_add_record', name="add_record_initial"),
+    url(r'^add_record/(?P<dns_server>[a-zA-Z0-9.-]+)/(?P<zone_name>[a-zA-Z0-9.-]+)/$', 'binder.views.view_add_record', name="add_record"),
     url(r'^add_record/result/$', 'binder.views.view_add_record_result', name="add_record_result"),
 
-    url(r'^delete_record/$', 'binder.views.view_delete_record', name="delete_record_initial"),
+    url(r'^delete_record/$', 'binder.views.view_delete_record', name="delete_record"),
     url(r'^delete_record/result/$', 'binder.views.view_delete_result', name="delete_record_result"),
 
-    url(r'^add_cname/(?P<dns_server>[a-zA-Z0-9.-]+)/(?P<zone_name>[a-zA-Z0-9.-]+)/(?P<record_name>[a-zA-Z0-9-]+)/$', 'binder.views.view_add_cname_record', name="add_cname_initial"),
+    url(r'^add_cname/(?P<dns_server>[a-zA-Z0-9.-]+)/(?P<zone_name>[a-zA-Z0-9.-]+)/(?P<record_name>.*?)/$', 'binder.views.view_add_cname_record', name="add_cname"),
     url(r'^add_cname_record/result/$', 'binder.views.view_add_cname_result', name="add_cname_result"),
 )
