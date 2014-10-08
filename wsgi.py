1ff21cb4abefefcf1d15cd4168d7b02977400490
Jeffrey Forman <jforman@interlaken.home.jeffreyforman.net>
2014-10-08 09:44:29 -0400

    add wsgi.py to binder project for use with apache

---
+++ wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for binder project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "binder.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
