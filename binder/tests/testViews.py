b62af7b64df78fb7b908465f789369cf7aceece1
jeffrey forman <jeff@jeffreyforman.net>
2015-04-04 22:07:49 -0400

    fix UI tests that werent updated to match the new templates

--- binder/tests/testViews.py
+++ binder/tests/testViews.py
@@ -52,13 +52,13 @@ class PostTests(TestCase):
                                                                 "rr_list" : [] })
 
         self.assertContains(response,
-                            '<input type="hidden" name="zone_name" value="testzone1.test.net">',
+                            '<input type="text" class="form-control hidden" name="zone_name" value="testzone1.test.net"/>',
                             html=True)
         self.assertContains(response,
-                            '<input type="hidden" name="rr_list" value="[]">',
+                            '<input type="text" class="form-control hidden" name="rr_list" value="[]"/>',
                             html=True)
         self.assertContains(response,
-                            '<input type="hidden" name="dns_server" value="testserver.test.net" />',
+                            '<input type="text" class="form-control hidden" name="dns_server" value="testserver.test.net"/>',
                             html=True)
 
 
@@ -68,13 +68,11 @@ class PostTests(TestCase):
                                                                "zone_name" : "testzone1.test.net",
                                                                "rr_list" : ["testrecord1.testzone1.test.net",
                                                                             "testrecord2.testzone1.test.net"] })
-
         self.assertContains(response,
-                            '<input type="hidden" name="zone_name" value="testzone1.test.net">',
+                            '<input type="text" class="form-control hidden" name="zone_name" value="testzone1.test.net"/>', html=True)
+        self.assertContains(response,
+                            '<input type="text" class="form-control hidden" name="rr_list" value="[u&#39;testrecord1.testzone1.test.net&#39;, u&#39;testrecord2.testzone1.test.net&#39;]"/>',
                             html=True)
         self.assertContains(response,
-                            '<input type="hidden" name="rr_list" value="[u\'testrecord1.testzone1.test.net\', u\'testrecord2.testzone1.test.net\']">',
-                            html=True)
-        self.assertContains(response,
-                            '<input type="hidden" name="dns_server" value="testserver.test.net" />',
+                            '<input type="text" class="form-control hidden" name="dns_server" value="testserver.test.net"/>',
                             html=True)
